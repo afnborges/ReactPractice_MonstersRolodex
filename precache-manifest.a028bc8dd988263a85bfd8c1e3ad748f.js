9534b291e38b6f694a15571dcc80e598e52d6f26
André Borges <afnborges@live.com.pt>
2019-08-03 18:17:26 +0100

    Updates

---
+++ precache-manifest.a028bc8dd988263a85bfd8c1e3ad748f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "507b91f41d7e8b12588e8fb3209da201",
+    "url": "/ReactPractice_MonstersRolodex/index.html"
+  },
+  {
+    "revision": "a67760fb4e3ced32ff03",
+    "url": "/ReactPractice_MonstersRolodex/static/css/main.aab8b305.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/ReactPractice_MonstersRolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a67760fb4e3ced32ff03",
+    "url": "/ReactPractice_MonstersRolodex/static/js/main.098ec8ce.chunk.js"
+  },
+  {
+    "revision": "35e781ec6beabae4cf9a",
+    "url": "/ReactPractice_MonstersRolodex/static/js/runtime~main.68239851.js"
+  }
+]);
